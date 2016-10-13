226c5bf88bcd4cb0a3a33704d88271eec651a73e
Bindu <bindus@thoughtworks.com>
2016-10-13 21:11:24 +0530

    Modified object format and added method to check whether a string passes or not

--- src/DFAGenerator.java
+++ src/DFAGenerator.java
@@ -4,11 +4,11 @@ import java.util.HashMap;
 public class DFAGenerator {
     public final ArrayList<String> states;
     private final ArrayList<String> alphabets;
-    private final HashMap<String, String> transition;
-    private final ArrayList<String> initialState;
+    private final HashMap<String, HashMap> transition;
+    private final String initialState;
     private final ArrayList<String> finalState;
 
-    public DFAGenerator(ArrayList<String> states, ArrayList<String> alphabets, HashMap<String, String> transition, ArrayList<String> initialState, ArrayList<String> finalState) {
+    public DFAGenerator(ArrayList<String> states, ArrayList<String> alphabets, HashMap<String, HashMap> transition, String initialState, ArrayList<String> finalState) {
         this.states = states;
         this.alphabets = alphabets;
         this.transition = transition;
@@ -16,11 +16,30 @@ public class DFAGenerator {
         this.finalState = finalState;
     }
 
-    public static DFAGenerator createDFA(ArrayList<String> states, ArrayList<String> alphabets, HashMap<String, String> transition, ArrayList<String> initialState, ArrayList<String> finalState) {
+    public static DFAGenerator createDFA(ArrayList<String> states, ArrayList<String> alphabets, HashMap<String, HashMap> transition, String initialState, ArrayList<String> finalState) {
         if (states.isEmpty() || alphabets.isEmpty() || transition.isEmpty() || initialState.isEmpty() || finalState.isEmpty())
             throw new NonEmptyArgumentException();
-        if (transition.values().contains(""))
+        if (transition.values().contains(new HashMap()))
             throw new NonEmptyArgumentException();
         return new DFAGenerator(states, alphabets, transition, initialState, finalState);
+    }
+
+    public boolean hasRecognizes(String s) {
+        String[] letters = s.split("");
+        String currentState = this.initialState;
+        for (String letter : letters)
+            currentState = (String) this.transition.get(currentState).get(letter);
+        return this.finalState.contains(currentState);
+    }
+
+    @Override
+    public String toString() {
+        return "DFAGenerator{" +
+                "states=" + states +
+                ", alphabets=" + alphabets +
+                ", transition=" + transition +
+                ", initialState=" + initialState +
+                ", finalState=" + finalState +
+                '}';
     }
 }
--- test/DFAGeneratorTest.java
+++ test/DFAGeneratorTest.java
@@ -12,38 +12,42 @@ public class DFAGeneratorTest {
 
     private ArrayList<String> states;
     private ArrayList<String> alphabets;
-    private HashMap<String, String> transition;
-    private ArrayList<String> initialState;
+    private HashMap<String, HashMap> transition;
+    private String initialState;
     private ArrayList<String> finalState;
 
     @Before
     public void setUp() throws Exception {
         states = new ArrayList<>();
         alphabets = new ArrayList<>();
-        initialState = new ArrayList<>();
         finalState = new ArrayList<>();
         transition = new HashMap<>();
 
         states.add("q1");
         states.add("q2");
-        states.add("q3");
 
         alphabets.add("0");
         alphabets.add("1");
 
-        initialState.add("q1");
+        initialState = "q1";
         finalState.add("q2");
 
-        transition.put("q10", "q2");
-        transition.put("q11", "q2");
-        transition.put("q20", "q2");
-        transition.put("q21", "q2");
+        HashMap hashMap0 = new HashMap<String, String>();
+        HashMap hashMap1 = new HashMap<String, String>();
+        hashMap0.put("0", "q2");
+        transition.put("q1", hashMap0);
+        hashMap0.put("1", "q1");
+        transition.put("q1", hashMap0);
+        hashMap1.put("0", "q1");
+        transition.put("q2", hashMap1);
+        hashMap1.put("1", "q2");
+        transition.put("q2", hashMap1);
     }
 
     @Test
     public void shouldCreateDFAObjectIfGivenFiveTupleIsValid() throws Exception {
-        DFAGenerator generator = DFAGenerator.createDFA(states, alphabets, transition, initialState, finalState);
-        assertEquals(generator.states, states);
+        DFAGenerator dfa = DFAGenerator.createDFA(states, alphabets, transition, initialState, finalState);
+        assertEquals(dfa.states, states);
     }
 
     @Rule
@@ -52,9 +56,15 @@ public class DFAGeneratorTest {
 
     @Test
     public void shouldThrowInvalidObjectCreationException() throws Exception {
-        transition.put("q10", "");
+        transition.put("q1", new HashMap());
         thrown.expect(NonEmptyArgumentException.class);
         thrown.expectMessage("Expected Non-empty argument");
         DFAGenerator.createDFA(states, alphabets, transition, initialState, finalState);
+    }
+
+    @Test
+    public void shouldPassAStringThatContainsOddNoOf0s() throws Exception {
+        DFAGenerator dfa = DFAGenerator.createDFA(states, alphabets, transition, initialState, finalState);
+        assertEquals(dfa.hasRecognizes("000"), true);
     }
 }
